f2e3644f9325d6b31974f6a7342a962174db88fd
Kahan Chauhan <139649383+Kahan99@users.noreply.github.com>
2026-03-23 20:50:35 +0530

    chore: delete file.chore: delete file.

--- src/components/common/CharacterModel.jsx
+++
@@ -1,7 +0,0 @@
-import { useGLTF } from "@react-three/drei"
-
-export default function CharacterModel(props) {
-  const { scene } = useGLTF("/models/character.glb") // Replace with your actual path
-
-  return <primitive object={scene} {...props} />
-}
